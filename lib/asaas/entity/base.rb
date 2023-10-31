5eadc83c2c7f6c88724b4f754a9b0c32d083634d
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-10-31 14:00:03 -0300

    feat: add more compatibility to v3

--- lib/asaas/entity/base.rb
+++ lib/asaas/entity/base.rb
@@ -3,6 +3,19 @@ module Asaas
     class Base
       include Virtus.model
 
+      # permite definir atributos dinamicamente
+      def initialize(attributes = {}, *args)
+        self.extend(Virtus.model)
+    
+        attribute_set.merge(self.class.attribute_set)
+    
+        attributes.keys.each do |name|
+          attribute(name) unless attribute_set[name] 
+        end
+    
+        super
+      end
+
       def empty?
         attributes.values.compact.empty?
       end
--- spec/anticipation_spec.rb
+++ spec/anticipation_spec.rb
@@ -1,34 +1,71 @@
 require 'webmock/rspec'
 require 'asaas-ruby'
 
-RSpec.describe Asaas::Customer do
+RSpec.describe Asaas::Api::ReceivableAnticipation do
+  let(:asaas_client) { Asaas::Client.new }
+  let(:headers) do 
+    {
+      'Access-Token' => Asaas::Configuration.token,
+      'Content-Type' => 'application/json',
+      'User-Agent' => 'Typhoeus - https://github.com/typhoeus/typhoeus'
+    }
+  end
+
+  let(:list_example_response) { File.read('spec/fixtures/list_anticipations.json') }
+  let(:simulate_example_response) { File.read('spec/fixtures/simulate_anticipation_success_response.json') }
+  let(:sign_agreement_example_response) { File.read('spec/fixtures/sign_agreement_success_response.json') }
+  let(:get_anticipation_example_response) { File.read('spec/fixtures/get_anticipation_success_response.json') }
+
   before do
     Asaas::Configuration.token = '544333290e93b9bbcd8107b3d9586e3bef774fb41584790a5ff061e4e0392ed5'
     Asaas::Configuration.api_version = 3
   end
 
-  let(:example_response) { File.read('spec/fixtures/list_anticipations.json') }
-
   it 'gets anticipations' do
-    asaas_client = Asaas::Client.new
-
     params = {}
 
     stub_request(:get, "https://sandbox.asaas.com/api/v3/anticipations")
-      .with(
-        query: params,
-        headers: {
-          'Access-Token' => Asaas::Configuration.token,
-          'Content-Type' => 'application/json',
-          'User-Agent' => 'Typhoeus - https://github.com/typhoeus/typhoeus'
-        }
-      )
-      .to_return(status: 200, body: example_response, headers: {'Content-Type' => 'application/json'})
+      .with(query: params, headers: headers)
+      .to_return(status: 200, body: list_example_response)
 
-    list = asaas_client.anticipations.list(params)
+    response = asaas_client.anticipations.list(params)
 
-    expect(list).to be_a(Asaas::Entity::Meta)
-    expect(list.data).to be_a(Array)
-    expect(list.data.first).to be_a(Asaas::ReceivableAnticipation)
+    expect(response).to be_a(Asaas::Entity::Meta)
+    expect(response.data).to be_a(Array)
+    expect(response.data.first).to be_a(Asaas::ReceivableAnticipation)
+  end
+
+  it 'get anticipation' do
+    stub_request(:get, "https://sandbox.asaas.com/api/v3/anticipations/id")
+      .with(query: {id: 'id'}, headers: headers)
+      .to_return(status: 200, body: get_anticipation_example_response)
+
+    response = asaas_client.anticipations.get('id')
+
+    expect(response).to be_a(Asaas::ReceivableAnticipation)
+  end
+
+  it 'simulate anticipation' do
+    body = {}
+
+    stub_request(:post, "https://sandbox.asaas.com/api/v3/anticipations/simulate")
+      .with(body: body, headers: headers)
+      .to_return(status: 200, body: simulate_example_response)
+
+    response = asaas_client.anticipations.simulate(body)
+
+    expect(response).to be_a(Asaas::Entity::Base)
+    expect(response.empty?).to be false
+  end
+
+  it 'sign agreement' do
+    stub_request(:post, "https://sandbox.asaas.com/api/v3/anticipations/agreement/sign")
+      .with(body: {agreed: true}.to_json, headers: headers)
+      .to_return(status: 200, body: sign_agreement_example_response)
+
+    response = asaas_client.anticipations.sign_agreement
+
+    expect(response).to be_a(Asaas::Entity::Base)
+    expect(response.empty?).to be false
   end
 end
